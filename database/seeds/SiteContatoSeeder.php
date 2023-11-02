9e898b4b67bcf56bf58e36bc416085dff2284727
Thiago Guerreiro <thiagoguerreiro.si@gmail.com>
2023-11-02 13:09:27 -0300

    Semeando tabelas em massa com a dependência Faker

--- database/seeds/SiteContatoSeeder.php
+++ database/seeds/SiteContatoSeeder.php
@@ -12,6 +12,7 @@ class SiteContatoSeeder extends Seeder
      */
     public function run()
     {
+        /*
         $contato = new SiteContato();
         $contato->nome = 'Sistema SG';
         $contato->telefone = '(11) 99999-9999';
@@ -19,5 +20,8 @@ class SiteContatoSeeder extends Seeder
         $contato->motivo_contato = 1;
         $contato->mensagem = 'Seja bem vindo ao sistema Super Gestão';
         $contato->save();
+        */
+
+        factory(SiteContato::class, 100)->create();
     }
 }
