d9487b7b4000287833aff6509a13f5f42e0ba615
Thiago Guerreiro <thiagoguerreiro.si@gmail.com>
2023-11-08 21:37:02 -0300

    Repopulando formulário com Request Old

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -9,38 +9,25 @@ class ContatoController extends Controller
 {
     public function contato(Request $request) {
 
-        /*
-        if(!(empty($request->all()))) {
-            $contato = new SiteContato();
-            $contato->nome = $request->input('nome');
-            $contato->telefone = $request->input('telefone');
-            $contato->email = $request->input('email');
-            $contato->motivo_contato = $request->input('motivo_contato');
-            $contato->mensagem = $request->input('mensagem');
+        $motivo_contatos = [
+            '1' => 'Dúvida',
+            '2' => 'Elogio',
+            '3' => 'Reclamação'
+        ];
 
-            $contato->save();
-        }
-        */
-
-        /*
-        if(!(empty($request->all()))) {
-            $contato = new SiteContato();
-            $contato->create($request->all());
-        }
-        */
-
-        return view('site.contato', ['titulo' => 'Contato (teste)']);
+        return view('site.contato', ['titulo' => 'Contato (teste)', 'motivo_contatos' => $motivo_contatos]);
     }
 
     public function salvar(Request $request) {
 
-        $request->validade([
+        $request->validate([
             'nome' => 'required|min:3|max:40',
             'telefone' => 'required',
             'email' => 'required',
             'motivo_contato' => 'required',
             'mensagem' => 'required'
         ]);
+
         //SiteContato::create($request->all());
     }
 }
