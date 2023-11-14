7f588955c37cfb1d26e2596211b2b2ecae10de80
Thiago Guerreiro <thiagoguerreiro.si@gmail.com>
2023-11-14 20:20:18 -0300

    Customização de mensagens de erro no formulário de contato

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -17,13 +17,24 @@ class ContatoController extends Controller
 
     public function salvar(Request $request) {
 
-        $request->validate([
-            'nome' => 'required|min:3|max:40',
+        $regras =             [
+            'nome' => 'required|min:3|max:40|unique:site_contatos',
             'telefone' => 'required',
             'email' => 'email',
             'motivo_contatos_id' => 'required',
-            'mensagem' => 'required'
-        ]);
+            'mensagem' => 'required|max:2000'
+        ];
+
+        $feedback = [
+            'nome.min' => 'O campo nome precisa ter no mínimo 3 caracteres',
+            'nome.max' => 'O campo nome deve ter no máximo 40 caracteres',
+            'email.email' => 'O email informado não é válido',
+            'mensagem.max' => 'A mensagem deve ter no máximo 2000 caracteres',
+
+            'required' => 'O campo :attribute deve ser preenchido'
+        ];
+
+        $request->validate($regras, $feedback);
 
         SiteContato::create($request->all());
         return redirect()->route('site.index');
