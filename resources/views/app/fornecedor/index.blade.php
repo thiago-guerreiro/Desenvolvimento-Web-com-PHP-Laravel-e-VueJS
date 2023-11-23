2844026f76b2caecf3a767e4efbdf64adeed15c1
Thiago Guerreiro <thiagoguerreiro.si@gmail.com>
2023-11-23 19:32:21 -0300

    Menu de opções da área protegida

--- resources/views/app/fornecedor/index.blade.php
+++
@@ -1,9 +0,0 @@
-<h3>Fornecedor</h3>
-
-@if(count($fornecedores) > 0 && count($fornecedores) < 10)
-    <h3>Existem alguns fornecedores cadastrados</h3>
-@elseif(count($fornecedores) > 10)
-    <h3>Existem vários fornecedores cadastrados</h3>
-@else
-    <h3>Ainda não fornecedores cadastrados</h3>
-@endif
