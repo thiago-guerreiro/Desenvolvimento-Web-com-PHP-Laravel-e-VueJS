9da4e362f22962f041583c109660ab637c146ab5
Thiago Guerreiro <thiagoguerreiro.si@gmail.com>
2023-11-24 19:21:51 -0300

    Cadastro de fornecedor, inclusão de novo registro

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -3,10 +3,47 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Fornecedor;
 
 class FornecedorController extends Controller
 {
     public function index() {
-        return view('app.fornecedor');
+        return view('app.fornecedor.index');
+    }
+
+    public function listar() {
+        return view('app.fornecedor.listar');
+    }
+
+    public function adicionar(Request $request) {
+
+        $msg = '';
+
+        if($request->input('_token') != '') {
+
+            $regras = [
+                'nome' => 'required|min:3|max:40',
+                'site' => 'required',
+                'uf' => 'required|min:2|max:2',
+                'email' => 'email'
+            ];
+
+            $feedback = [
+                'required' => 'O campo :attribute deve ser preenchida',
+                'nome.min' => 'O campo nome deve ter no mínimo 3 caracteres',
+                'nome.max' => 'O campo nome deve ter no máximo 40 caracteres',
+                'uf.min' => 'O campo uf deve ter no mínimo 2 caracteres',
+                'uf.max' => 'O campo uf deve ter no máximo 2 caracteres',
+                'email.email' => 'O campo e-mail não foi preenchido corretamente'
+            ];
+
+            $request->validate($regras, $feedback);
+
+            $fornecedor = new Fornecedor();
+            $fornecedor->create($request->all());
+
+            $msg = 'Cadastro realizado com sucesso';
+        }
+        return view('app.fornecedor.adicionar', ['msg' => $msg]);
     }
 }
--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -61,6 +61,7 @@ class LoginController extends Controller
     }
 
     public function sair() {
-        echo 'Sair';
+        session_destroy();
+        return redirect()->route('site.index');
     }
 }
