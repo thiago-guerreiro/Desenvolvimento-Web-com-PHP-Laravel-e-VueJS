6ef71d497f1c6aafa861189e382f7121ad146122
Thiago Guerreiro <thiagoguerreiro.si@gmail.com>
2023-12-12 19:28:04 -0300

    Cadastro de produtos (show)

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -70,7 +70,7 @@ class ProdutoController extends Controller
      */
     public function show(Produto $produto)
     {
-        //
+        return view('app.produto.show', ['produto' => $produto]);
     }
 
     /**
