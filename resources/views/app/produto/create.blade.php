f03fff528706d02d91652328cce3ec753ca135b0
Thiago Guerreiro <thiagoguerreiro.si@gmail.com>
2023-12-13 20:57:30 -0300

    Refatoração do formulário de criação e edição de produtos

--- resources/views/app/produto/create.blade.php
+++ resources/views/app/produto/create.blade.php
@@ -19,27 +19,8 @@
 
         <div class="informacao-pagina">
             <div style="width: 30%; margin-left: auto; margin-right: auto;">
-                <form method="post" action="{{ route('produto.store') }}">
-                    @csrf
-                    <input type="text" name="nome" value="{{ old('nome') }}" placeholder="Nome" class="borda-preta">
-                    {{ $errors->has('nome') ? $errors->first('nome') : '' }}
-
-                    <input type="text" name="descricao" value="{{ old('descricao') }}" placeholder="Descrição" class="borda-preta">
-                    {{ $errors->has('descricao') ? $errors->first('descricao') : '' }}
-
-                    <input type="text" name="peso" value="{{ old('peso') }}" placeholder="Peso" class="borda-preta">
-                    {{ $errors->has('peso') ? $errors->first('peso') : '' }}
-
-                    <select name="unidade_id">
-                        <option>-- Selecione a Unidade de Medida --</option>
-                        @foreach($unidades as $unidade)
-                            <option value="{{ $unidade->id }}">{{ $unidade->descricao }}</option>
-                        @endforeach
-                    </select>
-                    {{ $errors->has('unidade_id') ? $errors->first('unidade_id') : '' }}
-
-                    <button type="submit" class="borda-preta">Cadastrar</button>
-                </form>
+                @component('app.produto._components.form_create_edit', ['unidades' => $unidades])
+                @endcomponent
             </div>
         </div>
 
