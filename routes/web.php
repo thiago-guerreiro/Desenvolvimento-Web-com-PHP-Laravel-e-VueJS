ea7abf273330ccb7bfc4bc1aeccd621b612b4385
Thiago Guerreiro <thiagoguerreiro.si@gmail.com>
2023-12-14 20:59:06 -0300

    Implentado produto detalhes

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,8 @@ Route::middleware('autenticacao:padrao,visitante')->prefix('/app')->group(functi
     Route::get('/fornecedor/excluir/{id}', 'FornecedorController@excluir')->name('app.fornecedor.excluir');
 
     Route::resource('produto', 'ProdutoController');
+
+    Route::resource('produto-detalhe', 'ProdutoDetalheController');
 });
 
 Route::fallback(function() {
